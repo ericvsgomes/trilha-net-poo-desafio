81d0a2dc1c5c82fa8b58e1f7008f38ea5bb7c280
Eric Gomes <ericvsg13@gmail.com>
2024-05-09 10:03:06 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.Net.WebSockets;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("87999998888", "Lanterninha", "123456789", 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("Cobrinha");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone:");
+Smartphone iphone = new Iphone("87988889999", "X", "987654321", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
